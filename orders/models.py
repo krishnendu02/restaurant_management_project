705acfb2371d51cc06a5927de36005fe81b59d45
Krishnendu Manna <89826967+krishnendu02@users.noreply.github.com>
2025-09-30 11:54:38 +0530

    51233

--- orders/models.py
+++ orders/models.py
@@ -2,10 +2,16 @@ from django.db import models
 from .models import OrderStatus
 
 # Create your models here.
-class Coupon(models.model):
+class Coupon(models.Model):
     code = models.CharField(max_length = 20, unique = True)
     discount = models.DecimalField(max_digits=5, decimal_places = 2)
     created_at = models.DateTimeField(auto_now_add = True)
 
+class OrderStatus(models.Model):
+    name = models.CharField(max_length = 50, unique = True)
+
 def __str__(self):
     return self.code
+
+def __str__(self):
+    return self.name
