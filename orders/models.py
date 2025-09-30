83ca75a02186d0d020316fa764a37bc0472c0d2b
krishnendu02 <krishnendumanna02@gmail.com>
2025-09-30 11:25:10 +0530

    remove extra word

--- orders/models.py
+++ orders/models.py
@@ -4,7 +4,7 @@ from .models import OrderStatus
 # Create your models here.
 class Coupon(models.Model):
     code = models.CharField(max_length=20, unique=True)
-    discount = models.DecimalField(max_digits=5, decimal_places=2)  # example
+    discount = models.DecimalField(max_digits=5, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
